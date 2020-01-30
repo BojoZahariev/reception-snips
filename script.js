a99c77044573f5bcbe4609aed2d7b4b7cbace561
Bojo <bozhidar_zahariev@hotmail.com>
2020-01-30 16:13:47 +0000

    buttons func

---
+++ script.js
@@ -0,0 +1,21 @@
+const initialDiv = document.querySelector('#initial');
+const colleaguesDiv = document.querySelector('#colleagues');
+const visitorsDiv = document.querySelector('#visitors');
+const colleaguesBtn = document.querySelector('#colleaguesBtn');
+const visitorsBtn = document.querySelector('#visitorsBtn');
+
+colleaguesBtn.addEventListener('click', function(e) {
+  initialDiv.style.display = 'none';
+  colleaguesDiv.style.display = 'block';
+});
+
+visitorsBtn.addEventListener('click', function(e) {
+  initialDiv.style.display = 'none';
+  visitorsDiv.style.display = 'block';
+});
+
+backBtn.addEventListener('click', function(e) {
+  initialDiv.style.display = 'block';
+  visitorsDiv.style.display = 'none';
+  colleaguesDiv.style.display = 'none';
+});
