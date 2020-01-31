62f1bc953f7fd24f18d96824347ee77ec6946275
Bojo <bozhidar_zahariev@hotmail.com>
2020-01-31 10:19:45 +0000

    styling, focus

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const colleaguesDiv = document.querySelector('#colleagues');
 const visitorsDiv = document.querySelector('#visitors');
 const colleaguesBtn = document.querySelector('#colleaguesBtn');
 const visitorsBtn = document.querySelector('#visitorsBtn');
+const backBtn = document.querySelector('#backBtn');
+const input = document.querySelector('#input1');
+const inputVisitors = document.querySelector('#input1Visitors');
 
 colleaguesBtn.addEventListener('click', function(e) {
   initialDiv.style.display = 'none';
   colleaguesDiv.style.display = 'block';
+  backBtn.style.display = 'block';
+  input.focus();
 
   playSound();
 });
@@ -14,12 +19,15 @@ colleaguesBtn.addEventListener('click', function(e) {
 visitorsBtn.addEventListener('click', function(e) {
   initialDiv.style.display = 'none';
   visitorsDiv.style.display = 'block';
+  backBtn.style.display = 'block';
+  inputVisitors.focus();
 });
 
 backBtn.addEventListener('click', function(e) {
   initialDiv.style.display = 'block';
   visitorsDiv.style.display = 'none';
   colleaguesDiv.style.display = 'none';
+  backBtn.style.display = 'none';
 });
 
 const playSound = () => {
